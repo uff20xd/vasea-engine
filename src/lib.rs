36a7baa21791960d0b90af8c00e8da15a00975ae
uff20xd <uff20xd@proton.me>
2026-01-03 21:40:08 +0100

    k

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::{
     io::prelude::*,
     fs,
+    sync::Mutex,
 };
 use std::ffi::OsStr;
 use std::path::Path;
@@ -17,32 +18,16 @@ struct Pixel {
     r: Byte,
     g: Byte,
     b: Byte,
+    // pub inner: Mutex<InnerPixel>
 }
 
-struct Image<const WIDTH: usize, const HEIGHT: usize, F>
-    where F: Fn(usize, usize, usize, usize, usize) -> Pixel {
-    image: [[Pixel; WIDTH]; HEIGHT],
-    // x, y
-    next_pixel: (usize, usize),
-    file_name: fs::File,
-    // x, y, zoom, width, height
-    pixel_fn: F,
+#[derive(Default, Clone, Copy)]
+struct InnerPixel {
+    r: Byte,
+    g: Byte,
+    b: Byte,
 }
 
-impl<const WIDTH: usize, const HEIGHT: usize, F> Image<WIDTH, HEIGHT, F> 
-    where F: Fn(usize, usize, usize, usize, usize) -> Pixel {
-
-    pub fn new<T>(file_name: T, pixel_fn: F) -> Self
-    where T: AsRef<Path> {
-        Self {
-            image: [[Pixel::default(); WIDTH]; HEIGHT],
-            next_pixel: (0, 0),
-            file_name: fs::File::create(file_name).expect(&format!("File couldnt be created in Line: {}", line!())),
-            pixel_fn,
-        }
-    }
-    pub fn get_task() -> Task<F> { todo!() }
-}
 
 struct Task<F> 
     where F: Fn(usize, usize, usize, usize, usize) -> Pixel {
@@ -54,8 +39,83 @@ struct Task<F>
     height: usize,
 }
 
+pub struct Image<const WIDTH: usize, const HEIGHT: usize> {
+    image: Mutex<[[Pixel; WIDTH]; HEIGHT]>,
+    file: fs::File,
+}
+
+pub struct Shader<const WIDTH: usize, const HEIGHT: usize, F> 
+    where F: Fn(usize, usize, f64, usize, usize) -> Pixel {
+    // x, y, zoom, width, height
+    zoom: f64
+    pixel_fn: F,
+    image: Image<WIDTH, HEIGHT>,
+}
+
+impl<const WIDTH: usize, const HEIGHT: usize, F> Shader<WIDTH, HEIGHT, F> 
+    where F: Fn(usize, usize, f64, usize, usize) -> Pixel {
+
+    pub fn new() -> Self { todo!( ) }
+    pub fn get_task(x: usize, y: usize) -> Task<F> { 
+
+        let task = Task::new(
+            self.pixel_fn.clone(),
+            x,
+            y,
+        );
+
+        task
+    }
+    pub fn apply_shader(self) -> Self {
+        self
+    }
+}
+
+impl<const WIDTH: usize, const HEIGHT: usize, F> Image<WIDTH, HEIGHT> {
+
+    pub fn new<T>(file_name: T) -> Self
+    where T: AsRef<Path> {
+        Self {
+            image: Mutex::new([[Pixel::default(); WIDTH]; HEIGHT]),
+            file: fs::File::create(file_name).expect(&format!("File couldnt be created in Line: {}", line!())),
+        }
+    }
+    pub fn write(&self) -> Result<(), ()> {
+        let image = self.image.get();
+        let size: Vec<u8> = format!("{} {}\n", width, height).bytes().collect();
+        _ = self.file.write(&(b"P6\n")[..]);
+        _ = self.file.write(&size[..]);
+        _ = self.file.write(&(b"255\n")[..]);
+
+        for x in self.width {
+            for y in self.height {
+                let pixel = image[x][y];
+                self.file.write(&[pixel.r, pixel.g, pixel.b])
+            }
+        }
+    }
+}
+
 impl<F> Task<F>
     where F: Fn(usize, usize, usize, usize, usize) -> Pixel {
+    pub fn new(
+        function: F,
+        x: usize,
+        y: usize,
+        zoom: usize,
+        width: usize,
+        height: usize,
+    ) -> Task {
+        Self {
+            function,
+            x,
+            y,
+            zoom,
+            width,
+            height,
+        }
+    }
+
     #[inline(always)]
     fn execute(&self) -> Pixel {
         (self.function)(
@@ -65,5 +125,13 @@ impl<F> Task<F>
             self.width,
             self.height,
         )
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn todo() {
+        todo!()
     }
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -24,48 +24,52 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     for i in 0..height {
         for j in 0..width {
-            current_pixel += 1;
-            if (current_pixel % (scale * scale * 5000)) == 0 { println!( "Pixel {} out of {}", current_pixel, width * height)}
-            let x0 = zoom_mult * (((j as f64/width as f64)/ 2.0) - (j as f64/width as f64)) + x_shift;
-            let y0 = zoom_mult * (((i as f64/height as f64)/ 2.0) - (i as f64/height as f64)) + y_shift;
-            let r_mult: f64 = 0.2 - (0.1_f64 * (x0 + y0 - 1.0)).abs() + 0.2 - (0.1_f64 * (x0 + y0 - 0.0)).abs();
-            let g_mult: f64 = 0.2 - (0.1_f64 * (x0 + y0 - 1.0)).abs();
-            let b_mult: f64 = 0.8; 
-            let max_mult = r_mult.max(g_mult.max(b_mult));
 
-            let mut x = 0.0;
-            let mut y = 0.0;
-
-            let mut colour: f64 = 0.0;
-            let mut n = 0;
-            let max_iteration = 1000;
-
-            while (x*x + y*y <= 2.0*2.0) && (n < max_iteration) {
-                let x_temp = x*x - y*y + x0;
-
-                y = 2.0*x*y + y0;
-                x = x_temp;
-
-                n += 1;
-            }
-
-            //colour = (255 * (n/max_iteration)) as f64 / max_mult;
-            colour = n as f64;
-
-            //if colour < 10.0 {
-            //    colour += 120.0;
-            //}
-
-            let r = (colour * r_mult).round() as Byte; //
-            let g = (colour * g_mult).round() as Byte; //
-            let b = (colour * b_mult).round() as Byte; //
-
-
-            _ = output.write(&[r, g, b]);
         }
     }
 
 
     println!("Generating File: {}", name);
     Ok(())
+}
+
+// x, y, zoom, width, height
+fn mandel_brot_shader(x: usize, y: usize, zoom: f64, width: usize, height: usize) -> Pixel {
+    current_pixel += 1;
+    if (current_pixel % (scale * scale * 5000)) == 0 { println!( "Pixel {} out of {}", current_pixel, width * height)}
+    let x0 = zoom_mult * (((j as f64/width as f64)/ 2.0) - (j as f64/width as f64)) + x_shift;
+    let y0 = zoom_mult * (((i as f64/height as f64)/ 2.0) - (i as f64/height as f64)) + y_shift;
+    let r_mult: f64 = 0.2 - (0.1_f64 * (x0 + y0 - 1.0)).abs() + 0.2 - (0.1_f64 * (x0 + y0 - 0.0)).abs();
+    let g_mult: f64 = 0.2 - (0.1_f64 * (x0 + y0 - 1.0)).abs();
+    let b_mult: f64 = 0.8; 
+    let max_mult = r_mult.max(g_mult.max(b_mult));
+
+    let mut x = 0.0;
+    let mut y = 0.0;
+
+    let mut colour: f64 = 0.0;
+    let mut n = 0;
+    let max_iteration = 1000;
+
+    while (x*x + y*y <= 2.0*2.0) && (n < max_iteration) {
+        let x_temp = x*x - y*y + x0;
+
+        y = 2.0*x*y + y0;
+        x = x_temp;
+
+        n += 1;
+    }
+
+    //colour = (255 * (n/max_iteration)) as f64 / max_mult;
+    colour = n as f64;
+
+    //if colour < 10.0 {
+    //    colour += 120.0;
+    //}
+
+    let r = (colour * r_mult).round() as Byte; //
+    let g = (colour * g_mult).round() as Byte; //
+    let b = (colour * b_mult).round() as Byte; //
+
+
 }
