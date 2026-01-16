099b9413b405ebca6c7a648ebfb2bedde5b4e12b
uff20xd <donut121008@gmail.com>
2026-01-16 15:25:02 +0100

    k

--- tests/tests.rs
+++ tests/tests.rs
@@ -11,20 +11,16 @@ const XDIM: usize = SCALE * 16 * 40;
 const YDIM: usize = SCALE * 16 * 40;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let mut thread_pool = ThreadPool::new();
-    // let input = "./tests/input_photo.ppm";
-    // let out = "./out/output.ppm";
-    // dbg!(b" ");
-    // let zoom = 1200.2;
-    // let x_shift = -0.0;
-    // let y_shift = 0.64;
-    // let in_image = Image::new(XDIM, YDIM, 0);
-    // let shader = Shader::new(&mandel_brot_shader, zoom, x_shift, y_shift, in_image);
-    // let image = shader.apply_shader(&mut thread_pool);
-    // image.write(out);
-    
-    let test = Image::read_ppm("./out/output.ppm")?;
-    test.write("./out/test.ppm");
+    let mut thread_pool = ThreadPool::new();
+    let input = "./tests/eminem_test.ppm";
+    let out = "./out/output.ppm";
+    let zoom = 1200.2;
+    let x_shift = -0.0;
+    let y_shift = 0.64;
+    let in_image = Image::read_ppm(input)?;
+    let shader = Shader::new(&mandel_brot_shader, zoom, x_shift, y_shift, in_image);
+    let image = shader.apply_shader(&mut thread_pool);
+    image.write(out);
     
     Ok(())
 }
@@ -35,8 +31,8 @@ fn mandel_brot_shader(in_pixel: Pixel, x: usize, y: usize, width: usize, height:
     let zoom_mult = 1.0 / zoom;
     let x0 = zoom_mult * (((x as f64/width as f64)/ 2.0) - (x as f64/width as f64)) + x_shift;
     let y0 = zoom_mult * (((y as f64/height as f64)/ 2.0) - (y as f64/height as f64)) + y_shift;
-    let r_mult: f64 = 0.6 - (0.1_f64 * (x0 + y0 - 1.0)).abs() + 0.2 - (0.1_f64 * (x0 + y0 - 0.0)).abs();
-    let g_mult: f64 = 0.6 - (0.1_f64 * (x0 + y0 - 1.0)).abs();
+    let r_mult: f64 = 0.4 - (0.1_f64 * (x0 + y0 - 1.0)).abs() + 0.2 - (0.1_f64 * (x0 + y0 - 0.0)).abs();
+    let g_mult: f64 = 0.4 - (0.1_f64 * (x0 + y0 - 1.0)).abs();
     let b_mult: f64 = 0.1; 
     let max_mult = r_mult.max(g_mult.max(b_mult));
     let (in_r, in_g, in_b) = in_pixel.get_rgb();
@@ -64,9 +60,9 @@ fn mandel_brot_shader(in_pixel: Pixel, x: usize, y: usize, width: usize, height:
     //    colour += 120.0;
     //}
 
-    let r = Wrapping(in_r) - Wrapping((colour * r_mult).round() as Byte); //
-    let g = Wrapping(in_g) - Wrapping((colour * g_mult).round() as Byte); //
-    let b = Wrapping(in_b) - Wrapping((colour * b_mult).round() as Byte); //
+    let r = Wrapping(in_r) + Wrapping((colour * r_mult).round() as Byte); //
+    let g = Wrapping(in_g) + Wrapping((colour * g_mult).round() as Byte); //
+    let b = Wrapping(in_b) + Wrapping((colour * b_mult).round() as Byte); //
 
     Pixel::new(r.0, g.0, b.0)
 }
