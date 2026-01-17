61379500cef969bde1dc5e77a1f7b932810913a8
uff20xd <uff20xd@proton.me>
2026-01-17 12:48:36 +0100

    k

--- tests/tests.rs
+++ tests/tests.rs
@@ -6,7 +6,7 @@ use std::{
     num::Wrapping,
 };
 type Byte = u8;
-const SCALE: usize = 4;
+const SCALE: usize = 6;
 const XDIM: usize = SCALE * 16 * 40;
 const YDIM: usize = SCALE * 16 * 40;
 
@@ -15,9 +15,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = "./tests/eminem_test.ppm";
     let out = "./out/output.ppm";
     let zoom = 1200.2;
-    let x_shift = -0.0;
+    let x_shift = -0.01;
     let y_shift = 0.64;
-    let in_image = Image::read_ppm(input)?;
+    // let in_image = Image::read_ppm(input)?;
+    let in_image = Image::new(YDIM, XDIM , 255);
     let shader = Shader::new(&mandel_brot_shader, zoom, x_shift, y_shift, in_image);
     let image = shader.apply_shader(&mut thread_pool);
     image.write(out);
@@ -27,10 +28,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 // x, y, zoom, width, height
 fn mandel_brot_shader(in_pixel: Pixel, x: usize, y: usize, width: usize, height: usize, zoom: f64,  x_shift: f64, y_shift: f64) -> Pixel {
-    if (width*x + y)%(SCALE * SCALE * 5000) == 0 { println!( "Pixel {} out of {}", width*x + y, width * height)}
+    if (height*x + y)%(SCALE * SCALE * 5000) == 0 { println!( "Pixel {} out of {}", height*x + y, width * height)}
     let zoom_mult = 1.0 / zoom;
-    let x0 = zoom_mult * (((x as f64/width as f64)/ 2.0) - (x as f64/width as f64)) + x_shift;
-    let y0 = zoom_mult * (((y as f64/height as f64)/ 2.0) - (y as f64/height as f64)) + y_shift;
+    let scaling = width.min(height);
+    let x0 = zoom_mult * (((x as f64/ scaling as f64)/ 2.0) - (x as f64/ scaling as f64)) + x_shift;
+    let y0 = zoom_mult * (((y as f64/ scaling as f64)/ 2.0) - (y as f64/ scaling as f64)) + y_shift;
     let r_mult: f64 = 0.4 - (0.1_f64 * (x0 + y0 - 1.0)).abs() + 0.2 - (0.1_f64 * (x0 + y0 - 0.0)).abs();
     let g_mult: f64 = 0.4 - (0.1_f64 * (x0 + y0 - 1.0)).abs();
     let b_mult: f64 = 0.1; 
@@ -60,9 +62,14 @@ fn mandel_brot_shader(in_pixel: Pixel, x: usize, y: usize, width: usize, height:
     //    colour += 120.0;
     //}
 
-    let r = Wrapping(in_r) - Wrapping((colour * r_mult).round() as Byte); //
-    let g = Wrapping(in_g) - Wrapping((colour * g_mult).round() as Byte); //
-    let b = Wrapping(in_b) - Wrapping((colour * b_mult).round() as Byte); //
+    let r = Wrapping(in_r) - Wrapping((colour * r_mult).round() as Byte);
+    let g = Wrapping(in_g) - Wrapping((colour * g_mult).round() as Byte);
+    let b = Wrapping(in_b) - Wrapping((colour * b_mult).round() as Byte);
 
+    // let r = (in_r as f64 * (colour * 0.2).round() as Byte as f64 / 255.0).round() as Byte;
+    // let g = (in_g as f64 * (colour * 0.2).round() as Byte as f64 / 255.0).round() as Byte;
+    // let b = (in_b as f64 * (colour * 0.2).round() as Byte as f64 / 255.0).round() as Byte;
+    
+    // Pixel::new(r, g, b)
     Pixel::new(r.0, g.0, b.0)
 }
